378ad05f0ddb3eb6db1310b18e6ed30e34802d22
thmiyathmary <hayato1203_zero_giga@yahoo.co.jp>
2018-04-23 02:24:15 +0900

    ToDo機能の追加と削除機能追加
    今後の予定
    ・下部のボタン追加
    ・各Todoの編集
    ・保存機能追加
    ・見た目

--- js/model.js
+++
@@ -1,3 +0,0 @@
-const Todo = Backbone.Model.extend({
-  
-});
--- js/mvc.js
+++ js/mvc.js
@@ -1,82 +1,121 @@
+  // 参考 http://www.cyokodog.net/blog/backbone-js/
+  // 参考 https://tnakamura.hatenablog.com/entry/20111024/backbone
 const EVENT_SUBMITTED = 'event_submitted'
+const EVENT_TOGGLEALL = 'event_toggleAll'
 
 const ParentView = Backbone.View.extend({
-  initialize: () => {
+  el: $('#wholeform'),
+  initialize: function() {
     this.todoCollection = new TodoCollection()
+    // 入力欄と全ToDoのトグルボタン
+    this.topView = new TopView()
     // ToDo表示を行う表
-    this.todoTableView = new TodoTableView({collection: todoCollection})
-    // ToDoを追加するためのView
-    this.todoAddView = new TodoAddView()
-    // 一括でisCompletedを操作するcheckbox
-    this.toggleAllIsCompletedView = new ToggleAllIsCompletedView()
+    this.todoTableView = new TodoTableView({collection: this.todoCollection})
     // 画面下部のボタン群
-    this.bottomView = new TodoBottomView()
+    this.bottomView = new BottomView()
 
-    console.log("test")
-
-    this.todoAddView.on(EVENT_SUBMITTED, (val) => {
+    this.topView.on(EVENT_SUBMITTED, (val) => {
       console.log(val)
       const todo = new TodoModel({
-        name: val.name,
+        name: val,
         isCompleted: false
       })
+      this.todoCollection.add(todo)
+      console.log(this.todoCollection)
+    })
+    this.topView.on(EVENT_TOGGLEALL, (val) => {
+      console.log(val)
     })
   },
 
 })
 
-const TodoAddView = Backbone.View.extend({
-
-  initialize:() => {
-    this.$el = $('#wholeform')
-    console.log(this.$el)
-    console.log($('#wholeform'))
+const TopView = Backbone.View.extend({
+  el: '#top',
+  initialize: function() {
+    this.$input = this.$('#maintext')
+    this.$toggleAll = this.$('#toggleAll')
+    console.log(this)
   },
   events: {
-    'submit #maintext': 'onSubmitted',
-    'onclick #toggleAll': 'onClicked'
+    'keydown #maintext': 'onSubmitted',
+    'click #toggleAll': 'onClicked'
   },
-  onSubmitted: (e) => {
-    console.log(this.$el)
-    this.trigger(EVENT_SUBMITTED,this.val())
+  onSubmitted: function(e) {
+    if(e.keyCode === 13 && this.$input.val() !== ''){
+      this.trigger(EVENT_SUBMITTED, e.target.value)
+      e.target.value = ''
+    }
   },
-  onClicked: (e) => {
-    console.log(this.$el)
-    console.log(this.val())
-    // this.trigger(EVENT_SUBMITTED,this.val())
+  onClicked: function(e) {
+    this.trigger(EVENT_TOGGLEALL, e.target.checked)
   }
 })
 
 const TodoTableView = Backbone.View.extend({
+  el: 'table#todoList',
+  initialize: function() {
+    this.collection.bind("add", this.render, this)
+    this.collection.bind("remove", this.render, this)
+  },
+  render: function() {
+    this.$el.empty()
+    this.collection.each((todo) => {
+      const todoView = new TodoView({model: todo})
+      this.el.append(todoView.el)
+    })
+  }
 })
 
-const ToggleAllIsCompletedView = Backbone.View.extend({
-  initialize:() => {
-    this.$el = $('#toggleAll')
-    console.log(this.$el)
-    console.log($('#toggleAll'))
-    console.log(this.$el.val())
+const TodoView = Backbone.View.extend({
+  tagName: 'tr',
+  template: _.template($("#todo_template").html()),
+  model: '',
+  initialize:function() {
+    this.model.bind('change', this.render, this)
+    this.render()
+  },
+  render:function() {
+    this.$el.append(this.template(this.model.toJSON()))
   },
   events: {
-    'onclick': 'onClicked'
+    'click td.checkbox': 'onClickCheckbox',
+    'dblclick ': 'onDblClick',
+    'click td.remove': 'onClickRemove'
   },
-  onClicked: (e) => {
-    console.log(this.$el)
-    console.log(this.val())
-    // this.trigger(EVENT_SUBMITTED,this.val())
+  onClickCheckbox: function(e) {
+    this.model.isCompleted = e.target.checked
+  },
+  onDblClick: function() {
+
+  },
+  onClickRemove: function() {
+    this.model.destroy()
   }
 
 })
 
-const TodoModel = Backbone.Model.extend({
-
+const BottomView = Backbone.View.extend({
 })
 
-const TodoBottomView = Backbone.View.extend({
+const TodoModel = Backbone.Model.extend({
+  initialize: function() {
+    const isCompleted = false
+  }
 })
 
 const TodoCollection = Backbone.Collection.extend({
-  model: TodoModel
+  model: TodoModel,
+  initialize: function() {
+    this.bind("add", this.onAdd, this);
+  },
+  onAdd: function(task) {
+    task.bind("destroy", this.onDestroy, this);
+  },
+  onDestroy: function(task) {
+    this.remove(task);
+  }
+
 })
 
-const tmp = new ParentView()
+const parentView = new ParentView()
