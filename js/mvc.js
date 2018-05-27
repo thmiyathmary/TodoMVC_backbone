3673919430c0a6dba49b564cd91083a27e792fd2
thmiyathmary <hayato1203_zero_giga@yahoo.co.jp>
2018-05-28 01:27:11 +0900

    振る舞いは完成

--- js/mvc.js
+++ js/mvc.js
@@ -2,17 +2,18 @@
   // 参考 https://tnakamura.hatenablog.com/entry/20111024/backbone
 const EVENT_SUBMITTED = 'event_submitted'
 const EVENT_TOGGLEALL = 'event_toggleAll'
+const EVENT_SELECTRADIO = 'event_selectRadio'
 
 const ParentView = Backbone.View.extend({
   el: $('#wholeform'),
   initialize: function() {
     this.todoCollection = new TodoCollection()
     // 入力欄と全ToDoのトグルボタン
-    this.topView = new TopView()
+    this.topView = new TopView({collection: this.todoCollection})
     // ToDo表示を行う表
     this.todoTableView = new TodoTableView({collection: this.todoCollection})
     // 画面下部のボタン群
-    this.bottomView = new BottomView()
+    this.bottomView = new BottomView({collection: this.todoCollection})
 
     this.topView.on(EVENT_SUBMITTED, (val) => {
       console.log(val)
@@ -26,6 +27,10 @@ const ParentView = Backbone.View.extend({
     this.topView.on(EVENT_TOGGLEALL, (val) => {
       console.log(val)
     })
+    this.bottomView.on(EVENT_SELECTRADIO, (val) => {
+      this.todoTableView.status = val;
+      this.todoTableView.render();
+    })
   },
 
 })
@@ -35,6 +40,9 @@ const TopView = Backbone.View.extend({
   initialize: function() {
     this.$input = this.$('#maintext')
     this.$toggleAll = this.$('#toggleAll')
+    this.collection.bind("add", this.toggleChecked, this)
+    this.collection.bind("change", this.toggleChecked, this)
+    this.collection.bind("remove", this.toggleChecked, this)
     console.log(this)
   },
   events: {
@@ -48,23 +56,40 @@ const TopView = Backbone.View.extend({
     }
   },
   onClicked: function(e) {
-    this.trigger(EVENT_TOGGLEALL, e.target.checked)
+    const tmp = e.target.checked;
+    this.collection.each((model) => {model.set('isCompleted',tmp)})
+
+  },
+  toggleChecked: function() {
+    if(this.collection.length !== 0 && this.collection.filter((model) => {return model.attributes.isCompleted}).length === this.collection.length) {
+      this.$toggleAll[0].checked = true;
+    } else {
+      this.$toggleAll[0].checked = false;
+    }
   }
 })
 
 const TodoTableView = Backbone.View.extend({
   el: 'table#todoList',
+  status: 'All',
   initialize: function() {
     this.collection.bind("add", this.render, this)
+    this.collection.bind("change", this.render, this)
     this.collection.bind("remove", this.render, this)
   },
   render: function() {
-    this.$el.empty()
+    this.$el.empty();
     this.collection.each((todo) => {
       const todoView = new TodoView({model: todo})
+      if(this.status === 'All' || (this.status === 'Active' && !todo.attributes.isCompleted)  || (this.status === 'Completed' && todo.attributes.isCompleted)){
+        todoView.$el.show();
+      } else {
+        todoView.$el.hide();
+      }
       this.el.append(todoView.el)
     })
-  }
+  },
+
 })
 
 const TodoView = Backbone.View.extend({
@@ -76,6 +101,7 @@ const TodoView = Backbone.View.extend({
     this.render()
   },
   render:function() {
+    this.$el.empty();
     this.$el.append(this.template(this.model.toJSON()))
   },
   events: {
@@ -84,7 +110,8 @@ const TodoView = Backbone.View.extend({
     'click td.remove': 'onClickRemove'
   },
   onClickCheckbox: function(e) {
-    this.model.isCompleted = e.target.checked
+    // this.model.attributes.isCompleted = e.target.checked;
+    this.model.set('isCompleted',e.target.checked);
   },
   onDblClick: function() {
 
@@ -96,6 +123,29 @@ const TodoView = Backbone.View.extend({
 })
 
 const BottomView = Backbone.View.extend({
+  el: 'table#bottom',
+  initialize: function() {
+    this.collection.bind("add", this.checkShow, this)
+    this.collection.bind("change", this.checkShow, this)
+    this.collection.bind("remove", this.checkShow, this)
+  },
+  events: {
+    'click #radioForm': 'onSelectedRadio',
+    'click #clear': 'onClickClear'
+  },
+  checkShow: function() {
+    if(this.collection.length === 0) {
+      this.el.style.display='none';
+    } else {
+      this.el.style.display='inline-block';
+    }
+  },
+  onSelectedRadio: function(e) {
+    this.trigger(EVENT_SELECTRADIO, e.target.value)
+  },
+  onClickClear: function() {
+    this.collection.remove(this.collection.filter((model) => {return model.attributes.isCompleted}));
+  }
 })
 
 const TodoModel = Backbone.Model.extend({
